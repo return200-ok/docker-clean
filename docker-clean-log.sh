318586e76b62884a62dbf818c4a0eab794bc904e
levancao798 <53284451+levancao798@users.noreply.github.com>
2021-04-23 02:05:46 +0000

    Update docker-clean-log.sh

--- docker-clean-log.sh
+++ docker-clean-log.sh
@@ -1,1 +1,12 @@
+#To get the size of the logs
+sudo sh -c "du -ch /var/lib/docker/containers/*/*-json.log"
+#To only get the total of the size of the logs
+sudo sh -c "du -ch /var/lib/docker/containers/*/*-json.log | grep total"
+#To delete all logs
 sudo sh -c "truncate -s 0 /var/lib/docker/containers/*/*-json.log"
+#or
+>  /var/lib/docker/containers/*/*-json.log
+#or
+cat /dev/null > /var/lib/docker/containers/*/*-json.log
+#or
+echo "" > /var/lib/docker/containers/*/*-json.log
